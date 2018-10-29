e1df391712b1d822cdb73c4397d8cb228f64ad8f
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-29 14:42:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,9 @@ def my_select(collection)
  i = 0
  arr = []
  while i < collection.length
-   arr.push(collection.select(yield(collection[i])))
+   if collection[i] == "true"
+     arr.push(collection[i])
+   end
    i += 1
  end
  arr
